1d2d062ce3512feffeace734396392cc98041c22
Andre Sousa <up202005277@up.pt>
2023-03-01 15:37:14 +0000

    Fixed op precendence

--- src/main/pt/up/fe/comp2023/SimpleParser.java
+++ src/main/pt/up/fe/comp2023/SimpleParser.java
@@ -10,6 +10,8 @@ import pt.up.fe.comp.jmm.parser.JmmParserResult;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp.jmm.report.ReportType;
 import pt.up.fe.comp.jmm.report.Stage;
+import pt.up.fe.comp2023.JavammLexer;
+import pt.up.fe.comp2023.JavammParser;
 
 
 import java.util.Collections;
@@ -21,7 +23,7 @@ import java.util.Map;
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
  * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
+ * <a href="http://www.apache.org/licenses/LICENSE-2.0">...</a>
  * <p>
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
