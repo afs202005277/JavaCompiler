9f5af20bb8bc211cda9de7834bf3df5eb39bbb86
Andre Sousa <up202005277@up.pt>
2023-04-01 11:02:04 +0100

    working on the call method

---
+++ src/main/pt/up/fe/comp2023/JasminConverter.java
@@ -0,0 +1,148 @@
+package pt.up.fe.comp2023;
+
+import org.specs.comp.ollir.*;
+import pt.up.fe.comp.jmm.jasmin.JasminResult;
+import pt.up.fe.comp.jmm.ollir.OllirResult;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class JasminConverter implements pt.up.fe.comp.jmm.jasmin.JasminBackend {
+    private static final HashMap<String, String> typeToDescriptor = new HashMap<>() {{
+        put("BOOLEAN", "Z");
+        put("INT32", "I");
+        put("STRING", "Ljava/lang/String");
+        put("VOID", "V");
+    }};
+    private String handleType(String suffix, Descriptor variable) {
+        StringBuilder jasminCode = new StringBuilder();
+        // INT32, BOOLEAN, ARRAYREF, OBJECTREF, THIS,  STRING, VOID
+        String variableType = variable.getVarType().getTypeOfElement().name();
+        switch (variableType) {
+            case "THIS" -> jasminCode.append("a").append(suffix);
+            case "INT32" -> jasminCode.append("l").append(suffix);
+            case "BOOLEAN" -> jasminCode.append("a").append(suffix);
+            case "ARRAYREF" -> jasminCode.append("a").append(suffix);
+            case "OBJECTREF" -> jasminCode.append("a").append(suffix);
+            case "STRING" -> jasminCode.append("a").append(suffix);
+            case "VOID" -> jasminCode.append(suffix);
+        }
+        return jasminCode.toString();
+    }
+
+    private String handleType(Type type) {
+        StringBuilder jasminCode = new StringBuilder();
+        // INT32, BOOLEAN, ARRAYREF, OBJECTREF, THIS,  STRING, VOID
+        switch (type.getTypeOfElement().name()) {
+            case "THIS" -> jasminCode.append("a").append("return");
+            case "INT32" -> jasminCode.append("l").append("return");
+            case "BOOLEAN" -> jasminCode.append("a").append("return");
+            case "ARRAYREF" -> jasminCode.append("a").append("return");
+            case "OBJECTREF" -> jasminCode.append("a").append("return");
+            case "STRING" -> jasminCode.append("a").append("return");
+            case "VOID" -> jasminCode.append("return");
+        }
+        return jasminCode.toString();
+    }
+
+    @Override
+    public JasminResult toJasmin(OllirResult ollirResult) {
+        StringBuilder jasminCode = new StringBuilder();
+        ClassUnit ollirClassUnit = ollirResult.getOllirClass();
+
+        jasminCode.append(".class ").append(ollirClassUnit.getClassAccessModifier()).append(" ").append(ollirClassUnit.getClassName()).append("\n");
+
+        jasminCode.append(".super ").append(ollirClassUnit.getSuperClass()).append("\n\n\n");
+
+        for (Method method : ollirClassUnit.getMethods()) {
+            List<Instruction> instructions = method.getInstructions();
+            List<Element> params = method.getParams();
+            String staticStr = " static ";
+            if (!method.isStaticMethod()) {
+                staticStr = " ";
+            }
+            if (ollirClassUnit.getClassName().equals(method.getMethodName())){
+                jasminCode.append(".method public <init>");
+            } else{
+                jasminCode.append(".method ").append(method.getMethodAccessModifier()).append(staticStr).append(method.getMethodName());
+            }
+            jasminCode.append("(");
+            for (Element element : method.getParams()) {
+                Type type = element.getType();
+                if (type.getTypeOfElement().name().equals("ARRAYREF")){
+                    ArrayType tmp = (ArrayType) type;
+                    jasminCode.append("[").append(tmp.getElementClass()).append(";");
+                } else{
+                    jasminCode.append(element.getType().getTypeOfElement().name()).append(";");
+                }
+            }
+            jasminCode.append(")");
+            jasminCode.append(JasminConverter.typeToDescriptor.get(method.getReturnType().getTypeOfElement().name()));
+            jasminCode.append("\n");
+            if (!method.getVarTable().isEmpty()) {
+                jasminCode.append(".limit stack 99\n");
+                jasminCode.append(".limit locals 99\n");
+            }
+            for (Instruction instruction : instructions) {
+                switch (instruction.getInstType()) {
+                    case CALL -> jasminCode.append(processCall((CallInstruction) instruction, method.getVarTable()));
+                    case GOTO -> jasminCode.append(processGoTo((GotoInstruction) instruction));
+                    case NOPER -> jasminCode.append(processNoper(instruction));
+                    case ASSIGN -> jasminCode.append(processAssign((AssignInstruction) instruction));
+                    case BRANCH -> jasminCode.append(processBranch(instruction));
+                    case RETURN -> jasminCode.append(processReturn((ReturnInstruction) instruction));
+                    case GETFIELD -> jasminCode.append(processGetField((FieldInstruction) instruction));
+                    case PUTFIELD -> jasminCode.append(processPutField((FieldInstruction) instruction));
+                    case UNARYOPER -> jasminCode.append(processUnaryOp((UnaryOpInstruction) instruction));
+                    case BINARYOPER -> jasminCode.append(processBinaryOp((BinaryOpInstruction) instruction));
+                }
+            }
+            jasminCode.append(handleType(method.getReturnType())).append("\n");
+            jasminCode.append(".end method").append("\n\n");
+        }
+        return new JasminResult(jasminCode.toString());
+    }
+
+    private String processCall(CallInstruction instruction, HashMap<String, Descriptor> varTable) {
+        StringBuilder code = new StringBuilder();
+        if (! (instruction.getFirstArg().toString().equals("CLASS") || instruction.getFirstArg().toString().equals("VOID")))
+            code.append(handleType("load_" + varTable.get(instruction.getFirstArg().getType().getTypeOfElement().name().toLowerCase()).getVirtualReg(), varTable.get(instruction.getFirstArg().getType().getTypeOfElement().name().toLowerCase()))).append("\n");
+        return code.append(instruction.getInvocationType().name()).append(" ").append(instruction.getSecondArg()).append("()V").append("\n").toString();
+    }
+
+    private String processGoTo(GotoInstruction instruction) {
+        return null;
+    }
+
+    private String processNoper(Instruction instruction) {
+        return null;
+    }
+
+    private String processAssign(AssignInstruction instruction) {
+        return null;
+    }
+
+    private String processBranch(Instruction instruction) {
+        return null;
+    }
+
+    private String processReturn(ReturnInstruction instruction) {
+        return null;
+    }
+
+    private String processGetField(FieldInstruction instruction) {
+        return null;
+    }
+
+    private String processPutField(FieldInstruction instruction) {
+        return null;
+    }
+
+    private String processUnaryOp(UnaryOpInstruction instruction) {
+        return null;
+    }
+
+    private String processBinaryOp(BinaryOpInstruction instruction) {
+        return null;
+    }
+}
--- src/main/pt/up/fe/comp2023/Launcher.java
+++ src/main/pt/up/fe/comp2023/Launcher.java
@@ -1,11 +1,14 @@
 package pt.up.fe.comp2023;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
+import pt.up.fe.comp.jmm.ollir.OllirResult;
 import pt.up.fe.comp.jmm.parser.JmmParserResult;
 import pt.up.fe.comp.jmm.report.Report;
 import pt.up.fe.comp.jmm.report.ReportType;
@@ -13,10 +16,11 @@ import pt.up.fe.comp.jmm.report.Stage;
 import pt.up.fe.specs.util.SpecsIo;
 import pt.up.fe.specs.util.SpecsLogs;
 import pt.up.fe.specs.util.SpecsSystem;
+import pt.up.fe.comp2023.JasminConverter;
 
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // Setups console logging and other things
         SpecsSystem.programStandardInit();
 
@@ -34,8 +38,14 @@ public class Launcher {
         // Read contents of input file
         String code = SpecsIo.read(inputFile);
 
+        String content = new Scanner(new File("./test/pt/up/fe/comp/cp2/apps/example_ollir/HelloWorld.ollir")).useDelimiter("\\Z").next();
+        // System.out.println(content);
+        JasminConverter jasminConverter = new JasminConverter();
+        System.out.println(jasminConverter.toJasmin(new OllirResult(content, config)).getJasminCode());
+
+
         // Instantiate JmmParser
-        SimpleParser parser = new SimpleParser();
+        /*SimpleParser parser = new SimpleParser();
 
         // Parse stage
         JmmParserResult parserResult = parser.parse(code, config);
@@ -55,7 +65,7 @@ public class Launcher {
                 System.out.println(temp);
                 System.out.println('\n');
             }
-        }
+        }*/
     }
 
     private static Map<String, String> parseArgs(String[] args) {
